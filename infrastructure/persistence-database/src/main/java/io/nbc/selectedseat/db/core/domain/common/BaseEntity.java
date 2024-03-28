0ebd93ed3d673c7340bd1054c4ea399a95459502
DevFrog92 <yongsu1481@gmail.com>
2024-03-28 21:21:21 +0900

    feat: remove id field in BaseEntity

--- infrastructure/persistence-database/src/main/java/io/nbc/selectedseat/db/core/domain/common/BaseEntity.java
+++ infrastructure/persistence-database/src/main/java/io/nbc/selectedseat/db/core/domain/common/BaseEntity.java
@@ -14,11 +14,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-
     @Column
     @CreatedDate
     private LocalDateTime createAt;
