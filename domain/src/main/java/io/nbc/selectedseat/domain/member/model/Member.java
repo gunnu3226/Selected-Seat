cabb487be61cbf00ceb09b34b467e9e44be74018
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-03-29 00:41:29 +0900

    feat: #4 Member infrastructure base

--- domain/src/main/java/io/nbc/selectedseat/domain/member/model/Member.java
+++ domain/src/main/java/io/nbc/selectedseat/domain/member/model/Member.java
@@ -3,6 +3,7 @@ package io.nbc.selectedseat.domain.member.model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Member {
     // TODO: sample class
     private Long member_id;
@@ -19,8 +21,6 @@ public class Member {
     private String profile;
     private LocalDate birth;
     private MemberRole member_role;
-    private String tel;
-
     // TODO: add
 
     // TODO: update
--- infrastructure/persistence-database/src/main/java/io/nbc/selectedseat/db/core/domain/member/entity/MemberJpaEntity.java
+++ infrastructure/persistence-database/src/main/java/io/nbc/selectedseat/db/core/domain/member/entity/MemberJpaEntity.java
@@ -2,67 +2,81 @@ package io.nbc.selectedseat.db.core.domain.member.entity;
 
 import io.nbc.selectedseat.db.core.domain.common.BaseEntity;
 import io.nbc.selectedseat.domain.member.model.Member;
+import io.nbc.selectedseat.domain.member.model.MemberRole;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
 public class MemberJpaEntity extends BaseEntity {
 
-    @Column
-    private String name;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long member_id;
 
     @Column
     private String email;
 
     @Column
     private String password;
-
+    
     @Column
-    private String tel;
+    private String nickname;
+    
+    @Column
+    private String profile;
+    
+    @Column
+    private LocalDate birth;
+    
+    @Column
+    @Enumerated(EnumType.STRING)
+    private MemberRole member_role;
 
     public MemberJpaEntity() {
     }
 
     public MemberJpaEntity(
-        String name,
         String email,
         String password,
-        String tel
+        String nickname,
+        String profile,
+        LocalDate birth,
+        MemberRole memberRole
     ) {
-        this.name = name;
         this.email = email;
         this.password = password;
-        this.tel = tel;
+        this.nickname = nickname;
+        this.profile = profile;
+        this.birth = birth;
+        this.member_role = memberRole;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 
     public static MemberJpaEntity from(Member member) {
         return new MemberJpaEntity(
-            member.getName(),
             member.getEmail(),
             member.getPassword(),
-            member.getTel()
+            member.getNickname(),
+            member.getProfile(),
+            member.getBirth(),
+            member.getMember_role()
         );
     }
 
     public Member toModel() {
         return new Member(
-            1L,
-            name,
+            member_id,
             email,
-            tel,
-            password
+            password,
+            nickname,
+            profile,
+            birth,
+            member_role
         );
     }
 }
