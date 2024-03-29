ea88d279cebce5bf6fe2f3e755173bd8a159e6a6
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-03-29 22:21:11 +0900

    feat: #15 followArtist repository feature

--- domain/src/main/java/io/nbc/selectedseat/domain/member/repository/FollowRepository.java
+++ domain/src/main/java/io/nbc/selectedseat/domain/member/repository/FollowRepository.java
@@ -1,5 +1,8 @@
 package io.nbc.selectedseat.domain.member.repository;
 
+import io.nbc.selectedseat.domain.member.model.Follow;
+
 public interface FollowRepository {
 
+    Follow save(Follow follow);
 }
--- infrastructure/persistence-database/src/main/java/io/nbc/selectedseat/db/core/domain/member/adaptor/FollowPersistenceAdaptor.java
+++ infrastructure/persistence-database/src/main/java/io/nbc/selectedseat/db/core/domain/member/adaptor/FollowPersistenceAdaptor.java
@@ -1,6 +1,8 @@
 package io.nbc.selectedseat.db.core.domain.member.adaptor;
 
+import io.nbc.selectedseat.db.core.domain.member.entity.FollowEntity;
 import io.nbc.selectedseat.db.core.domain.member.repository.FollowJpaRepository;
+import io.nbc.selectedseat.domain.member.model.Follow;
 import io.nbc.selectedseat.domain.member.repository.FollowRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,4 +13,8 @@ public class FollowPersistenceAdaptor implements FollowRepository {
 
     private final FollowJpaRepository followJpaRepository;
 
+    @Override
+    public Follow save(Follow follow) {
+        return followJpaRepository.save(FollowEntity.from(follow)).toModel();
+    }
 }
