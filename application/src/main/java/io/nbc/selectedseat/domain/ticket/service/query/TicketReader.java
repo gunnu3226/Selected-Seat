99d0b1cae77279dc22f9f4e21744ec4f73dbc021
DevFrog92 <yongsu1481@gmail.com>
2024-03-29 20:27:26 +0900

    feat: #16 implement ticket search feature
    - add getTickets and getTicketsByConcertId
        - TicketPersistenceAdapter and TicketReader
    - TicketReader
    - add findAllByConcertId method in TicketJpaRepository

--- application/src/main/java/io/nbc/selectedseat/domain/ticket/service/query/TicketReader.java
+++ application/src/main/java/io/nbc/selectedseat/domain/ticket/service/query/TicketReader.java
@@ -1,5 +1,41 @@
 package io.nbc.selectedseat.domain.ticket.service.query;
 
+import io.nbc.selectedseat.domain.ticket.dto.TicketInfo;
+import io.nbc.selectedseat.domain.ticket.model.Ticket;
+import io.nbc.selectedseat.domain.ticket.repository.TicketRepository;
+import io.nbc.selectedseat.domain.ticket.service.exception.CustomTicketException;
+import io.nbc.selectedseat.domain.ticket.service.exception.TicketExceptionCode;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class TicketReader {
 
+    private final TicketRepository ticketRepository;
+
+
+    public List<TicketInfo> getTickets() {
+        return ticketRepository.getTickets()
+            .stream()
+            .map(TicketInfo::from)
+            .toList();
+    }
+
+    public TicketInfo getTicket(final Long ticketId) {
+        Ticket ticket = ticketRepository.getTicket(ticketId)
+            .orElseThrow(() -> new CustomTicketException(TicketExceptionCode.NOT_FOUND));
+        System.out.println(ticket.toString());
+        return TicketInfo.from(ticket);
+    }
+
+    public List<TicketInfo> getTicketsByConcertId(final Long concertId) {
+        return ticketRepository.getTicketsByConvertId(concertId)
+            .stream()
+            .map(TicketInfo::from)
+            .toList();
+    }
 }
--- infrastructure/persistence-database/src/main/java/io/nbc/selectedseat/db/core/domain/ticket/repository/TicketJpaRepository.java
+++ infrastructure/persistence-database/src/main/java/io/nbc/selectedseat/db/core/domain/ticket/repository/TicketJpaRepository.java
@@ -1,8 +1,12 @@
 package io.nbc.selectedseat.db.core.domain.ticket.repository;
 
 import io.nbc.selectedseat.db.core.domain.ticket.entity.TicketEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketJpaRepository extends JpaRepository<TicketEntity, Long> {
 
+    List<TicketEntity> findAllByConcertId(final Long concertId);
+
+    void deleteAllByConcertId(final Long concertId);
 }
