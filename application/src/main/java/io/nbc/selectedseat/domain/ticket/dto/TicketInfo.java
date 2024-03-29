a566dd13a0a9c83bf731ee722cdd320c9f4ed7fa
DevFrog92 <yongsu1481@gmail.com>
2024-03-29 20:30:06 +0900

    feat: #16 implement TicketInfo dto

--- application/src/main/java/io/nbc/selectedseat/domain/ticket/dto/TicketInfo.java
+++ application/src/main/java/io/nbc/selectedseat/domain/ticket/dto/TicketInfo.java
@@ -1,7 +1,24 @@
 package io.nbc.selectedseat.domain.ticket.dto;
 
+import io.nbc.selectedseat.domain.ticket.model.Ticket;
+import io.nbc.selectedseat.domain.ticket.model.TicketRating;
+import java.time.LocalDateTime;
+
 public record TicketInfo(
-    Long ticketNumber
+    Long ticketId,
+    Long concertId,
+    TicketRating ticketRating,
+    String ticketNumber,
+    LocalDateTime createdAt
 ) {
 
+    public static TicketInfo from(final Ticket ticket) {
+        return new TicketInfo(
+            ticket.getTicketId(),
+            ticket.getConcertId(),
+            ticket.getTicketRating(),
+            ticket.getTicketNumber(),
+            ticket.getCreatedAt()
+        );
+    }
 }
