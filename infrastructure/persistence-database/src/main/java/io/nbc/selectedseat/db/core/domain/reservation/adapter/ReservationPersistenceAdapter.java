b7d7e13e56d3f3f8bf0adb0fd8c67148eee62264
DevFrog92 <yongsu1481@gmail.com>
2024-03-30 00:50:48 +0900

    test: #29 #30 add reservation persistence adapter

---
+++ infrastructure/persistence-database/src/main/java/io/nbc/selectedseat/db/core/domain/reservation/adapter/ReservationPersistenceAdapter.java
@@ -0,0 +1,42 @@
+package io.nbc.selectedseat.db.core.domain.reservation.adapter;
+
+import io.nbc.selectedseat.db.core.domain.reservation.entity.ReservationEntity;
+import io.nbc.selectedseat.db.core.domain.reservation.repository.ReservationJpaRepository;
+import io.nbc.selectedseat.domain.reservation.model.Reservation;
+import io.nbc.selectedseat.domain.reservation.repository.ReservationRepository;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class  ReservationPersistenceAdapter implements ReservationRepository {
+
+    private final ReservationJpaRepository reservationJpaRepository;
+
+
+    @Override
+    public Long createReservation(Reservation reservation) {
+        return reservationJpaRepository.save(ReservationEntity.from(reservation))
+            .getReservationId();
+    }
+
+    @Override
+    public Optional<Reservation> getReservation(Long id) {
+        return reservationJpaRepository.findById(id)
+            .map(ReservationEntity::toModel);
+    }
+
+    @Override
+    public List<Reservation> getReservations() {
+        return reservationJpaRepository.findAll().stream()
+            .map(ReservationEntity::toModel)
+            .toList();
+    }
+
+    @Override
+    public void deleteReservation(Long id) {
+        reservationJpaRepository.deleteById(id);
+    }
+}
