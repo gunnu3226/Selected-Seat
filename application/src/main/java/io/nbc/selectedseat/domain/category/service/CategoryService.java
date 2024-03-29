e6bddad4683edb6cedde5549452de55e042cd9c1
Son JunHyung <kingjh1125@gmail.com>
2024-03-29 23:57:22 +0900

    feat: #31 add delete endpoint - add `/api/v1/categorys/{categoryId}`

--- application/src/main/java/io/nbc/selectedseat/domain/category/service/CategoryService.java
+++ application/src/main/java/io/nbc/selectedseat/domain/category/service/CategoryService.java
@@ -3,6 +3,7 @@ package io.nbc.selectedseat.domain.category.service;
 import io.nbc.selectedseat.domain.category.exception.CategoryExistException;
 import io.nbc.selectedseat.domain.category.model.Category;
 import io.nbc.selectedseat.domain.category.repository.CategoryRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,12 +25,23 @@ public class CategoryService {
     }
 
     public Long updateCategory(final Long categoryId, final String name) {
-        if (categoryRepository.findById(categoryId).isEmpty()) {
-            throw new CategoryExistException("카테고리가 존재하지 않습니다");
-        }
+        validateCategory(categoryId);
 
         Category updatedcategory = categoryRepository.update(new Category(categoryId, name));
         return updatedcategory.getCategoryId();
     }
 
+    public void deleteCategory(final Long categoryId) {
+        Category category = validateCategory(categoryId);
+
+        categoryRepository.delete(category);
+    }
+
+    private Category validateCategory(final Long categoryId) {
+        Optional<Category> category = categoryRepository.findById(categoryId);
+        if (category.isEmpty()) {
+            throw new CategoryExistException("카테고리가 존재하지 않습니다");
+        }
+        return category.get();
+    }
 }
