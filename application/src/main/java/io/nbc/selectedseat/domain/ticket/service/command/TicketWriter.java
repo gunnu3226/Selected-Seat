ce150e5a8f6c9bfc22f79613e19ba479bcc315c4
DevFrog92 <yongsu1481@gmail.com>
2024-03-29 15:12:25 +0900

    feat: #2 implement ticket application feature
    - TicketWriter
    - TicketReader
    - TicketInfo

---
+++ application/src/main/java/io/nbc/selectedseat/domain/ticket/service/command/TicketWriter.java
@@ -0,0 +1,57 @@
+package io.nbc.selectedseat.domain.ticket.service.command;
+
+import static io.nbc.selectedseat.domain.ticket.model.TicketRating.A;
+import static io.nbc.selectedseat.domain.ticket.model.TicketRating.R;
+import static io.nbc.selectedseat.domain.ticket.model.TicketRating.S;
+
+import io.nbc.selectedseat.domain.ticket.model.Ticket;
+import io.nbc.selectedseat.domain.ticket.model.TicketRating;
+import io.nbc.selectedseat.domain.ticket.repository.TicketRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class  TicketWriter {
+
+    private final TicketRepository ticketRepository;
+
+    // TODO: import concertRepository
+    // TODO: import concertHallRepository
+    // TODO: ticket rating id > table replace to enum?
+    public Long createTicket(Long concertId) {
+        // TODO: will query from concertRepository
+        Long numOfSeats = 1000L;
+        Long numOfRow = 10L;
+
+        // TODO: will query from concertHallRepository
+        Long numOfRRatingTicket = 200L;
+        Long numOfSRatingTicket = 400L;
+        Long numOfARatingTicket = 400L;
+
+        generateTicketsByRating(numOfRRatingTicket, numOfRow, R);
+        generateTicketsByRating(numOfSRatingTicket, numOfRow, S);
+        generateTicketsByRating(numOfARatingTicket, numOfRow, A);
+
+        return numOfSeats;
+    }
+
+    private void generateTicketsByRating(
+        Long numOfSeats,
+        Long numOfRow,
+        TicketRating ticketRating
+    ) {
+        for (int i = 1; i < numOfSeats + 1; i++) {
+            String ticketNumber = i / numOfRow + "행:" + i % numOfRow + "열";
+
+            ticketRepository.save(
+                Ticket.builder()
+                    .ticketNumber(ticketNumber)
+                    .ticketRating(ticketRating)
+                    .build()
+            );
+        }
+    }
+}
---
+++ application/src/main/java/io/nbc/selectedseat/domain/ticket/service/query/TicketReader.java
@@ -0,0 +1,5 @@
+package io.nbc.selectedseat.domain.ticket.service.query;
+
+public class TicketReader {
+
+}
