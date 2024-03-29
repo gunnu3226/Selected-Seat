535fecca62c8cef4848e593fef22dfbfcd851f76
DevFrog92 <yongsu1481@gmail.com>
2024-03-29 20:29:09 +0900

    feat: #16 implement ticket search web entrypoint
    - TicketController
    - NumberOfTicketResponseDTO
    - TicketInfoResponseDTO

--- web/src/main/java/io/nbc/selectedseat/web/domain/ticket/TicketController.java
+++ web/src/main/java/io/nbc/selectedseat/web/domain/ticket/TicketController.java
@@ -1,14 +1,19 @@
 package io.nbc.selectedseat.web.domain.ticket;
 
+import io.nbc.selectedseat.domain.ticket.dto.TicketInfo;
 import io.nbc.selectedseat.domain.ticket.service.command.TicketWriter;
+import io.nbc.selectedseat.domain.ticket.service.query.TicketReader;
 import io.nbc.selectedseat.web.common.dto.ResponseDTO;
 import io.nbc.selectedseat.web.domain.ticket.dto.request.TicketCreateRequestDTO;
+import io.nbc.selectedseat.web.domain.ticket.dto.response.NumberOfTicketResponseDTO;
 import io.nbc.selectedseat.web.domain.ticket.dto.response.TicketInfoResponseDTO;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,16 +26,45 @@ import org.springframework.web.bind.annotation.RestController;
 public class TicketController {
 
     private final TicketWriter ticketWriter;
+    private final TicketReader ticketReader;
+
+    @GetMapping
+    public ResponseEntity<ResponseDTO<List<TicketInfoResponseDTO>>> getTickets() {
+        List<TicketInfoResponseDTO> tickets = ticketReader.getTickets().stream()
+            .map(TicketInfoResponseDTO::from)
+            .toList();
+
+        return ResponseEntity.ok(ResponseDTO.<List<TicketInfoResponseDTO>>builder()
+            .statusCode(HttpStatus.OK.value())
+            .message("전체 좌석이 조회되었습니다.")
+            .data(tickets)
+            .build()
+        );
+    }
+
+    @GetMapping("/{ticketId}")
+    public ResponseEntity<ResponseDTO<TicketInfoResponseDTO>> getTicket(
+        @PathVariable("ticketId") Long ticketId
+    ) {
+        TicketInfo ticket = ticketReader.getTicket(ticketId);
+
+        return ResponseEntity.ok(ResponseDTO.<TicketInfoResponseDTO>builder()
+            .statusCode(HttpStatus.OK.value())
+            .message("좌석이 조회되었습니다.")
+            .data(TicketInfoResponseDTO.from(ticket))
+            .build()
+        );
+    }
 
     @PostMapping
-    public ResponseEntity<ResponseDTO<?>> createTicket(
+    public ResponseEntity<ResponseDTO<NumberOfTicketResponseDTO>> createTicket(
         @Validated @RequestBody TicketCreateRequestDTO requestDTO
     ) {
-        TicketInfoResponseDTO responseDTO = TicketInfoResponseDTO.from(
+        NumberOfTicketResponseDTO responseDTO = NumberOfTicketResponseDTO.from(
             ticketWriter.createTicket(requestDTO.concertId())
         );
 
-        return ResponseEntity.ok(ResponseDTO.<TicketInfoResponseDTO>builder()
+        return ResponseEntity.ok(ResponseDTO.<NumberOfTicketResponseDTO>builder()
             .statusCode(HttpStatus.OK.value())
             .message("좌석이 등록되었습니다")
             .data(responseDTO)
@@ -41,7 +75,7 @@ public class TicketController {
     @DeleteMapping("/{ticketId}")
     public ResponseEntity<ResponseDTO<Void>> deleteTicket(
         @PathVariable("ticketId") Long ticketId
-    ){
+    ) {
         ticketWriter.deleteTicket(ticketId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
--- web/src/main/java/io/nbc/selectedseat/web/domain/ticket/dto/response/TicketInfoResponseDTO.java
+++ web/src/main/java/io/nbc/selectedseat/web/domain/ticket/dto/response/TicketInfoResponseDTO.java
@@ -1,10 +1,26 @@
 package io.nbc.selectedseat.web.domain.ticket.dto.response;
 
+import io.nbc.selectedseat.domain.ticket.dto.TicketInfo;
+import io.nbc.selectedseat.domain.ticket.model.TicketRating;
+import java.time.LocalDateTime;
+import lombok.Builder;
+
+@Builder
 public record TicketInfoResponseDTO(
-    Long numOfTickets
+    Long ticketId,
+    Long concertId,
+    TicketRating ticketRating,
+    String ticketNumber,
+    LocalDateTime createdAt
 ) {
 
-    public static TicketInfoResponseDTO from(Long numOfTickets) {
-        return new TicketInfoResponseDTO(numOfTickets);
+    public static TicketInfoResponseDTO from(final TicketInfo ticketInfo) {
+        return TicketInfoResponseDTO.builder()
+            .ticketId(ticketInfo.ticketId())
+            .concertId(ticketInfo.concertId())
+            .ticketRating(ticketInfo.ticketRating())
+            .ticketNumber(ticketInfo.ticketNumber())
+            .createdAt(ticketInfo.createdAt())
+            .build();
     }
 }
