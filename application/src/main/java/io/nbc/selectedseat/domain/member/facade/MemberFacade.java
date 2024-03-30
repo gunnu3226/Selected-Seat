970cdddeadac368b2377db75324494959bc36402
Lee gun woo <139452702+gunnu3226@users.noreply.github.com>
2024-03-31 01:05:36 +0900

    feat: #49 add readFollowArtists endpoint

--- application/src/main/java/io/nbc/selectedseat/domain/member/facade/MemberFacade.java
+++ application/src/main/java/io/nbc/selectedseat/domain/member/facade/MemberFacade.java
@@ -2,8 +2,8 @@ package io.nbc.selectedseat.domain.member.facade;
 
 import io.nbc.selectedseat.domain.artist.model.Artist;
 import io.nbc.selectedseat.domain.artist.service.ArtistService;
+import io.nbc.selectedseat.domain.member.facade.dto.FollowArtistsInfo;
 import io.nbc.selectedseat.domain.member.service.FollowService;
-import io.nbc.selectedseat.domain.member.service.MemberService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,13 +14,15 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberFacade {
 
-    private final MemberService memberService;
     private final FollowService followService;
     private final ArtistService artistService;
 
     @Transactional(readOnly = true)
-    public List<Artist> getFollowArtists(final Long memberId) {
+    public List<FollowArtistsInfo> getFollowArtists(final Long memberId) {
         List<Long> followArtistIds = followService.getFollowArtistIds(memberId);
-
+        List<Artist> artists = artistService.getArtistsByIds(followArtistIds);
+        return artists.stream()
+            .map(FollowArtistsInfo::from)
+            .toList();
     }
 }
