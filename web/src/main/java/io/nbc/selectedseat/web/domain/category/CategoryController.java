af029c9ee85e7f6c25a147c6d7b0901863a79cc0
Son JunHyung <kingjh1125@gmail.com>
2024-03-30 11:18:49 +0900

    refactor: #43 change singular to plural before: - `/api/v1/categorys/` after: - `/api/v1/categories/`

--- web/src/main/java/io/nbc/selectedseat/web/domain/category/CategoryController.java
+++ web/src/main/java/io/nbc/selectedseat/web/domain/category/CategoryController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/categorys")
+@RequestMapping("/api/v1/categories")
 @RequiredArgsConstructor
 public class CategoryController {
 
