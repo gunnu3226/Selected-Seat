17a09b54a4f74c312cd4571cb1638e5f4939cade
Son JunHyung <kingjh1125@gmail.com>
2024-03-30 11:16:31 +0900

    feat: #43 add getCategories endpoint - add `/api/v1/categorys/`

--- web/src/main/java/io/nbc/selectedseat/web/domain/category/CategoryController.java
+++ web/src/main/java/io/nbc/selectedseat/web/domain/category/CategoryController.java
@@ -1,14 +1,17 @@
 package io.nbc.selectedseat.web.domain.category;
 
+import io.nbc.selectedseat.domain.category.dto.GetCategoryResponseDTO;
 import io.nbc.selectedseat.domain.category.service.CategoryService;
 import io.nbc.selectedseat.web.common.dto.ResponseDTO;
 import io.nbc.selectedseat.web.domain.category.dto.request.CategoryRequestDTO;
 import io.nbc.selectedseat.web.domain.category.dto.response.CategoryResponseDTO;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -64,7 +67,6 @@ public class CategoryController {
         );
     }
 
-    //TODO : delete, list
     @DeleteMapping("/{categoryId}")
     public ResponseEntity<Void> deleteCategory(
         //TODO : @AuthenticationPrincipal UserDetailsImpl userDetails,
@@ -75,6 +77,18 @@ public class CategoryController {
         categoryService.deleteCategory(categoryId);
 
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseDTO<List<GetCategoryResponseDTO>>> getCategories(
+    ) {
+        return ResponseEntity.ok().body(
+            ResponseDTO.<List<GetCategoryResponseDTO>>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("카테고리 조회가 완료되었습니다")
+                .data(categoryService.getCategories())
+                .build()
+        );
     }
 
 
