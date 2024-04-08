8f508401868127dd455db037168ff5cbfe895047
yiyaaa <65538799+RamuneOrch@users.noreply.github.com>
2024-04-08 14:29:40 +0900

    #87 chage ec2 to elastic container service (#88)
    feat: #87 change ec2 to elastic container service
    
    refactor: #87 refactor conflict

---
+++ sh/mkdir.sh
@@ -0,0 +1,8 @@
+if [ ! -d "/var/lib/jenkins/workspace/Selected-Seat/infrastructure/persistence-database/src/main/resources/" ]; then
+    mkdir -p "/var/lib/jenkins/workspace/Selected-Seat/infrastructure/persistence-database/src/main/resources/"
+fi
+
+
+if [ ! -d "/var/lib/jenkins/workspace/Selected-Seat/infrastructure/persistence-redis-adapter/src/main/resources/" ]; then
+    mkdir -p "/var/lib/jenkins/workspace/Selected-Seat/infrastructure/persistence-redis-adapter/src/main/resources/"
+fi
