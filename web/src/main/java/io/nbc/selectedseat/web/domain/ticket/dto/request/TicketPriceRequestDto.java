fae01ff4506255f1a80d1774f622c98aa70eb484
DevFrog92 <yongsu1481@gmail.com>
2024-04-09 10:00:08 +0900

    refactor: #95 replace ticket rating enum to String

--- web/src/main/java/io/nbc/selectedseat/web/domain/ticket/dto/request/TicketPriceRequestDto.java
+++ web/src/main/java/io/nbc/selectedseat/web/domain/ticket/dto/request/TicketPriceRequestDto.java
@@ -1,13 +1,12 @@
 package io.nbc.selectedseat.web.domain.ticket.dto.request;
 
-import io.nbc.selectedseat.domain.ticket.model.TicketRating;
 import jakarta.validation.constraints.NotNull;
 
 public record TicketPriceRequestDto(
     @NotNull(message = "콘서트 아이디는 필수 입력값 입니다")
     Long concertId,
     @NotNull(message = "좌석 등급은 필수 입력값 입니다")
-    TicketRating ticketRating,
+    String ticketRating,
     @NotNull(message = "가격은 필수 입력값 입니다")
     Long price
 ) {
