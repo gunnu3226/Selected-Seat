f9fb4fd27ede35c959f24b4f60480ec6750281db
Son JunHyung <kingjh1125@gmail.com>
2024-04-16 21:41:30 +0900

    feat: #123 category security setting

--- web/src/main/java/io/nbc/selectedseat/web/domain/category/admin/CategoryAdminController.java
+++ web/src/main/java/io/nbc/selectedseat/web/domain/category/admin/CategoryAdminController.java
@@ -1,18 +1,15 @@
-package io.nbc.selectedseat.web.domain.category;
+package io.nbc.selectedseat.web.domain.category.admin;
 
-import io.nbc.selectedseat.domain.category.dto.GetCategoryResponseDTO;
 import io.nbc.selectedseat.domain.category.service.command.CategoryWriter;
-import io.nbc.selectedseat.domain.category.service.query.CategoryReader;
 import io.nbc.selectedseat.web.common.dto.ResponseDTO;
 import io.nbc.selectedseat.web.domain.category.dto.request.CategoryRequestDTO;
 import io.nbc.selectedseat.web.domain.category.dto.response.CategoryResponseDTO;
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -23,18 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/categories")
 @RequiredArgsConstructor
-public class CategoryController {
+public class CategoryAdminController {
 
     private final CategoryWriter categoryWriter;
-    private final CategoryReader categoryReader;
 
+    @Secured("ROLE_ADMIN")
     @PostMapping
     public ResponseEntity<ResponseDTO<CategoryResponseDTO>> createCategory(
-        //TODO : @AuthenticationPrincipal UserDetailsImpl userDetails,
         @RequestBody @Valid CategoryRequestDTO requestDTO
     ) {
-        //TODO : userDetails.getUser(); admin check
-
         CategoryResponseDTO responseDTO = CategoryResponseDTO.from(
             categoryWriter.createCategory(requestDTO.name())
         );
@@ -48,14 +42,12 @@ public class CategoryController {
         );
     }
 
+    @Secured("ROLE_ADMIN")
     @PutMapping("/{categoryId}")
     public ResponseEntity<ResponseDTO<CategoryResponseDTO>> updateCategory(
-        //TODO : @AuthenticationPrincipal UserDetailsImpl userDetails,
         @PathVariable Long categoryId,
         @RequestBody @Valid CategoryRequestDTO requestDTO
     ) {
-        //TODO : userDetails.getUser(); admin check
-
         CategoryResponseDTO responseDTO = CategoryResponseDTO.from(
             categoryWriter.updateCategory(categoryId, requestDTO.name())
         );
@@ -69,29 +61,14 @@ public class CategoryController {
         );
     }
 
+    @Secured("ROLE_ADMIN")
     @DeleteMapping("/{categoryId}")
     public ResponseEntity<Void> deleteCategory(
-        //TODO : @AuthenticationPrincipal UserDetailsImpl userDetails,
         @PathVariable Long categoryId
     ) {
-        //TODO : userDetails.getUser(); admin check
-
         categoryWriter.deleteCategory(categoryId);
 
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
-
-    @GetMapping
-    public ResponseEntity<ResponseDTO<List<GetCategoryResponseDTO>>> getCategories(
-    ) {
-        return ResponseEntity.ok().body(
-            ResponseDTO.<List<GetCategoryResponseDTO>>builder()
-                .statusCode(HttpStatus.OK.value())
-                .message("카테고리 조회가 완료되었습니다")
-                .data(categoryReader.getCategories())
-                .build()
-        );
-    }
-
 
 }
---
+++ web/src/main/java/io/nbc/selectedseat/web/domain/category/user/CategoryController.java
@@ -0,0 +1,34 @@
+package io.nbc.selectedseat.web.domain.category.user;
+
+import io.nbc.selectedseat.domain.category.dto.GetCategoryResponseDTO;
+import io.nbc.selectedseat.domain.category.service.query.CategoryReader;
+import io.nbc.selectedseat.web.common.dto.ResponseDTO;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryReader categoryReader;
+
+    @GetMapping
+    public ResponseEntity<ResponseDTO<List<GetCategoryResponseDTO>>> getCategories(
+    ) {
+        return ResponseEntity.ok().body(
+            ResponseDTO.<List<GetCategoryResponseDTO>>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("카테고리 조회가 완료되었습니다")
+                .data(categoryReader.getCategories())
+                .build()
+        );
+    }
+
+
+}
