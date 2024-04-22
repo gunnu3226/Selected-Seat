03cb58bd1f5e0cd3b551662dcb5516813a80d764
DevFrog92 <yongsu1481@gmail.com>
2024-04-22 20:39:50 +0900

    feat: #143 update search view

---
+++ ui/src/views/search/SearchView.vue
@@ -0,0 +1,179 @@
+<template>
+  <section class="py-5">
+    <div class="container px-4 px-lg-5">
+      <div class="search-bar-wrapper">
+        <span class="filter-title">키워드</span>
+        <input type="text" v-model="keyword" class="search-input">
+      </div>
+      <div>
+        <span class="filter-title">지역</span>
+        <label class="check-label">서울</label>
+        <input class="check-item" type="checkbox" value="서울" v-model="region">
+        <label class="check-label">경기/인천</label>
+        <input class="check-item" type="checkbox" value="경기/인천"
+               v-model="region">
+        <label class="check-label">대전/충청/강원</label>
+        <input class="check-item" type="checkbox" value="대전/충청/강원"
+               v-model="region">
+        <label class="check-label">부산/대구/경상</label>
+        <input class="check-item" type="checkbox" value="부산/대구/경상"
+               v-model="region">
+        <label class="check-label">광주/전라/제주</label>
+        <input class="check-item" type="checkbox" value="광주/전라/제주"
+               v-model="region">
+      </div>
+      <div>
+        <span class="filter-title">판매 상태</span>
+        <label class="check-label">판매 예정</label>
+        <input class="check-item" type="checkbox" value="판매 예정"
+               v-model="states">
+        <label class="check-label">판매중</label>
+        <input class="check-item" type="checkbox" value="판매중" v-model="states">
+        <label class="check-label">판매 종료</label>
+        <input class="check-item" type="checkbox" value="판매 종료"
+               v-model="states">
+      </div>
+      <div>
+        <span class="filter-title">관람 등급</span>
+        <label class="check-label">전체 관람가</label>
+        <input class="check-item" type="checkbox" value="전체 관람가"
+               v-model="concertRating">
+        <label class="check-label">미취학아동입장불가</label>
+        <input class="check-item" type="checkbox" value="미취학아동입장불가"
+               v-model="concertRating">
+        <label class="check-label">15세 이상 관람가</label>
+        <input class="check-item" type="checkbox" value="15세 이상 관람가"
+               v-model="concertRating">
+        <label class="check-label">19세 이상 관람가</label>
+        <input class="check-item" type="checkbox" value="19세 이상 관람가"
+               v-model="concertRating">
+      </div>
+      <div class="d-flex align-items-baseline">
+        <span class="filter-title">카테고리</span>
+        <span class="category-wrapper">
+        <label class="check-label">아이돌</label>
+        <input class="check-item" type="checkbox" value="아이돌"
+               v-model="categories">
+        <label class="check-label">팬클럽/팬미팅</label>
+        <input class="check-item" type="checkbox" value="팬클럽/팬미팅"
+               v-model="categories">
+        <label class="check-label">발라드/R&B</label>
+        <input class="check-item" type="checkbox" value="발라드/R&B"
+               v-model="categories">
+        <label class="check-label">힙합/EDM</label>
+        <input class="check-item" type="checkbox" value="힙합/EDM"
+               v-model="categories">
+        <label class="check-label">페스티벌</label>
+        <input class="check-item" type="checkbox" value="페스티벌"
+               v-model="categories">
+        <label class="check-label">인디/록</label>
+        <input class="check-item" type="checkbox" value="인디/록"
+               v-model="categories">
+        <label class="check-label">내한</label>
+        <input class="check-item" type="checkbox" value="내한"
+               v-model="categories">
+        <label class="check-label">그 외</label>
+        <input class="check-item" type="checkbox" value="그 외"
+               v-model="categories">
+        </span>
+      </div>
+      <div class="search-btn-wrapper">
+        <button class="btn btn-primary" @click="searchConcert"> 검색</button>
+      </div>
+      <hr>
+      <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4">
+        <div v-for="concertInfo in concertList" :key="concertInfo.title">
+					<span class="concert-info-wrapper">
+						<ConcertItem
+                :concertInfo="concertInfo"
+                @click="goToDetail(concertInfo.concertId)"
+            ></ConcertItem>
+					</span>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import {useRoute, useRouter} from "vue-router";
+import ConcertItem from "@/components/concert/ConcertItem.vue";
+import {ref} from "vue";
+
+const router = useRouter();
+const region = ref([]);
+const states = ref([]);
+const concertRating = ref([]);
+const categories = ref([]);
+const route = useRoute();
+const keyword = ref("");
+const concertList = ref([]);
+
+keyword.value = route.query.keyword;
+const searchConcert = async () => {
+  console.log(Object.keys(region.value).map(key => region.value[key]));
+  console.log(Object.keys(states.value.values()).map(key => region.value[key]));
+  console.log(
+      Object.keys(concertRating.value.values()).map(key => region.value[key]));
+  console.log(
+      Object.keys(categories.value.values()).map(key => region.value[key]));
+  console.log(keyword.value);
+}
+
+const goToDetail = id => {
+  router.push({
+    name: 'ConcertDetailView',
+    params: {
+      id,
+    },
+  });
+};
+</script>
+
+<style scoped>
+
+.filter-title {
+  width: 100px;
+  display: inline-block;
+  font-weight: 500;
+  margin-bottom: .5rem;
+}
+
+.check-label {
+  margin-right: .5rem;
+  flex-shrink: 0;
+}
+
+.check-item {
+  margin-right: 1rem;
+  width: 40px;
+  border-right: 1px solid lightgray;
+}
+
+.search-btn-wrapper {
+  margin-top: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.search-btn-wrapper button {
+  width: 100px;
+}
+
+.search-input {
+  padding-left: .5rem;
+  width: 80%;
+  flex-grow: 1;
+  height: 40px;
+}
+
+.search-bar-wrapper {
+  margin-bottom: 1rem;
+}
+
+.category-wrapper {
+  width: 80%;
+  display: inline-block;
+}
+</style>
